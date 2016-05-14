ceecbf36e3d6521347fe7f2f4e5fdc8eb513aaf3
sujitthuruthy <sujitthuruthy@gmail.com>
2016-05-14 20:01:05 +1000

    Section 1 completed

---
+++ R_Udemy.R
@@ -0,0 +1,20 @@
+# Section 1 - Read csv file by allowing user to select file from directory
+# using the explorer 
+myfile <- read.csv(file.choose())
+
+# Get the structure of the data loaded
+str(myfile)
+
+# Get statistical summary on each of the variables in the data
+summary(myfile)
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Plot scatter plot - filter carats more that 2.5 
+ggplot(data=myfile[myfile$carat<2.5,],
+       aes(x=carat, y=price, color=clarity)) + 
+  geom_point(alpha=0.1) +
+  geom_smooth()
+
+# Section 3 - R Fundamentals 
